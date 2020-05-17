f8ed96aee92ff6d93ecebbc8b48077cbd5cbaede
NSQA <nguyensyquanganh@gmail.com>
2020-05-17 23:19:54 +0700

    Update css & js

--- scripts/todo.script.js
+++ scripts/todo.script.js
@@ -60,57 +60,112 @@ inputField.addEventListener('keydown', (e) => {
 const handleCheckTask = (node) => {
     let inputChild = node.children[0];
     let taskInput = node.children[1];
-    let icon = node.children[2];
+    let icon = node.children[3];
+    
     node.style.cursor = 'default';
-    inputChild.style.cursor = 'default';
-    taskInput.style.cursor = 'default';
-    icon.style.visibility = 'visible';
-    if (!inputChild.checked) cnt--;
-    inputChild.checked = true;   
-    addCounting(cnt);
-    inputChild.disabled = true;
     node.style.backgroundColor = 'rgb(245, 101, 101)';
+    
+    inputChild.style.cursor = 'default';
+    inputChild.checked = true;   
+    inputChild.disabled = true;
+    
+    taskInput.style.cursor = 'default';
     taskInput.style.backgroundColor = 'rgb(245, 101, 101)';
+    
+    icon.style.visibility = 'visible';
+    
+    if (!inputChild.checked) cnt--;
+    
+    node.children[2].style.visibility = 'hidden';
+    node.children[4].style.visibility = 'hidden';
+        
+    addCounting(cnt);
 }
 
 var notInput = true;
 
 document.addEventListener('click', (e) => {
     if (e.target){
+        //Labels clicked
         if (e.target.tagName === 'LABEL') {
             handleCheckTask(e.target.parentNode);
         }
+        //List tasks clicked
         if (e.target.id === 'block-div') {
             if (notInput) handleCheckTask(e.target);
         }
+        //Checkbox ticked
         if (e.target.id.includes('task')) {
             e.target.checked = false;
             if (notInput) handleCheckTask(e.target.parentNode);
         }
+        //Delete icon clicked
         if (e.target.id.includes('trash-icon')) {
             let blockDivNode = e.target.parentNode;
+            
             blockDivNode.style.transform = 'translateX(200%)';
             blockDivNode.style.transition = 'transform .5s';
             blockDivNode.style.transitionTimingFunction = 'ease';
+
             setTimeout(() => {blockDivNode.remove();}, 500);
         }
+        //Edit icon clicked
         if (e.target.id.includes('edit-icon')) {
             notInput = false;
             let blockDivNode = e.target.parentNode;
-            let taskInp = blockDivNode.children[1];
-            //console.log(taskInp);
+            let taskInp = blockDivNode.children[2];
+            let label = blockDivNode.children[1];
+
             blockDivNode.style.cursor = 'default';
+            blockDivNode.style.backgroundColor = 'rgb(245, 101, 101)';
+
             blockDivNode.children[0].style.cursor = 'default';
             blockDivNode.children[0].disabled = true;
+           
+
+            label.style.visibility = 'hidden';
+
             taskInp.disabled = false;
-            taskInp.focus();
+            taskInp.style.visibility = 'visible';
             taskInp.style.cursor = 'text';
             taskInp.style.backgroundColor = 'rgb(255, 255, 254)';
+            taskInp.focus();
+            taskInp.select();
+            
             e.target.style.visibility = 'hidden';
         }
     }
     
 });
+
+//Press enter to save after editing
+document.addEventListener('keydown', (e) => {
+    if (e.target.id.includes('task-field') && e.keyCode === 13) {
+        notInput = true;
+
+        let inputField = e.target;
+        let blockDivNode = inputField.parentNode;
+        let label = blockDivNode.children[1];
+        inputField.value = inputField.value;
+        
+        label.innerHTML = inputField.value;
+        label.style.visibility = '';
+        blockDivNode.children[2].style.visibility = 'hidden';
+        
+        inputField.disabled = true;
+        inputField.style.backgroundColor = '';
+        inputField.style.cursor = 'pointer';
+        
+        blockDivNode.style.cursor = 'pointer';
+        blockDivNode.style.backgroundColor = '';
+
+        blockDivNode.children[0].style.cursor = 'pointer';
+        blockDivNode.children[0].disabled = false;
+        
+        blockDivNode.children[4].style.visibility = 'visible';
+
+    }
+})
 
 //Add present weekday and date
 
@@ -122,15 +177,19 @@ datenow = datenow.toString().slice(0,15);
 
 //Handle add task to the list
 const addTasks = (cnt) => {
+
     let div = document.createElement('div');
     div.id = 'block-div';
+
     let task = document.createElement('input');
     task.setAttribute('type', 'checkbox');
     task.id = 'task'+cnt;
     task.class = 'tasks';
-    // let label = document.createElement('label');
-    // label.setAttribute('for', 'task'+cnt);
-    // label.innerHTML = inputField.value;
+
+    let label = document.createElement('label');
+    label.setAttribute('for', 'task'+cnt);
+    label.innerHTML = inputField.value;
+
     let valueField = document.createElement('input');
     valueField.id = 'task-field'+cnt;
     valueField.className = 'task-field';
@@ -144,11 +203,12 @@ const addTasks = (cnt) => {
     let editIcon = document.createElement('span');
     editIcon.className = 'far fa-edit edit-icon';
     editIcon.id = "edit-icon"+cnt;
-    div.appendChild(task);
-    
-    div.appendChild(valueField);
-    div.appendChild(icon);
-    div.appendChild(editIcon);
+
+    div.appendChild(task); //children[0]
+    div.appendChild(label);//children[1]
+    div.appendChild(valueField);//children[2]
+    div.appendChild(icon);//children[3]
+    div.appendChild(editIcon);//children[4]
     listTasks.appendChild(div);
     
 }
